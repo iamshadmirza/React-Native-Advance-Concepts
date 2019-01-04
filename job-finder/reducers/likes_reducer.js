ddf0c79f36eba72c85e7bb033b34b8153c97c609
iamshadmirza <md.shad1@yahoo.com>
2019-01-04 07:12:53 +0530

    redux-persist

--- job-finder/reducers/likes_reducer.js
+++ job-finder/reducers/likes_reducer.js
@@ -1,8 +1,11 @@
 import { LIKE_JOB, CLEAR_LIKED_JOBS } from '../actions/types';
+import { REHYDRATE } from 'redux-persist/es/constants';
 import _ from 'lodash';
 
 export default function (state = [], action) {
     switch (action.type) {
+        case REHYDRATE:
+            return action.payload.likedJobs || [];
         case LIKE_JOB:
             return _.uniqBy([
                 action.payload, ...state
